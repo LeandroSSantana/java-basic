a0b508c478d1f346147f60e22ae40f001b59d76e
Leandro Santana <leandrosantana403@gmail.com>
2024-05-15 08:31:08 -0300

    Submissão do Desafio DIO

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+        
+        System.out.println("Digite o número da conta: ");
+        int numero = scanner.nextInt();
+        
+        System.out.println("Digite seu Nome: ");
+        scanner.nextLine(); // Consumindo a nova linha deixada pelo nextInt()
+        String nome = scanner.nextLine();
+        
+        System.out.println("Digite seu Saldo: ");
+        double saldo = scanner.nextDouble();
+
+        // Imprimindo os dados obtidos pelo Correntista
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
