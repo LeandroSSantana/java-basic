5cb98a5900f419ba70f446e3627602eadc5d084e
Leandro Santana <leandrosantana403@gmail.com>
2024-06-06 20:05:14 -0300

    Adicionando método entrandoEmContato

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,43 @@
 package candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        imprimirSelecionados();
+        String[] candidatosSelecionados = { "Ana", "Bruno", "Carlos", "Daniela", "Eduardo" };
+        // primeiro um for para selecionar os candidatos
+        for (String candidato : candidatosSelecionados) {
+            entrandoEmContato(candidato);
+        }
+    }
+
+    static void entrandoEmContato(String candidato) {
+
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando)
+                tentativasRealizadas++;
+            else
+                System.out.println("Contato realizado com sucesso");
+
+        } while (continuarTentando && tentativasRealizadas < 3);
+
+        if (atendeu)
+            System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + " tentativa");
+        else
+            System.out.println("Não conseguimos contato com " + candidato + ", número máximo de tentativas realizadas "
+                    + tentativasRealizadas);
+
+    }
+
+    // método auxiliar
+    static boolean atender() {
+        return new Random().nextInt(3) == 1;
     }
 
     static void imprimirSelecionados() {
